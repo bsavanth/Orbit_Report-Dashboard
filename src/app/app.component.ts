856cff5cc6b637fc65d811033f83dfc2c3c6391d
bsavanth <39973983+bsavanth@users.noreply.github.com>
2021-01-09 14:15:02 -0600

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+  title = 'orbit-report';
+  sourceList: Satellite[];
+  displayList: Satellite[];
+
+constructor() {
+  this.sourceList = [];
+  this.displayList=this.sourceList;
+  let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+  window.fetch(satellitesUrl).then(function(response) {
+     response.json().then(function(data) {
+
+        let fetchedSatellites = data.satellites;
+        
+        let i: number=0;
+        for (i=0; i<fetchedSatellites.length; i++)
+        {   
+            this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational));
+        }
+
+        this.displayList = this.sourceList.slice(0);
+     }.bind(this));
+  }.bind(this));
+
+}
+
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  
+  this.displayList = matchingSatellites;
+}
+
+
+}
---
+++ src/app/satellite.ts
@@ -0,0 +1,31 @@
+export class Satellite {
+
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+    debrischeck:boolean;
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean)
+    {
+        this.name = name;
+        this.type = type;
+        this.launchDate =launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+        this.debrischeck = this.shouldShowWarning();
+    }
+
+   
+    shouldShowWarning():boolean
+    {
+        if(((this.type).toLowerCase())==="space debris")
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+   
+}
